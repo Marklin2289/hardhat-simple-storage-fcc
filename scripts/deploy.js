36f819110d31221029ac9d3ddf836a0449ed7168
Mark Lin <61925580+Marklin2289@users.noreply.github.com>
2022-06-16 18:25:13 -0700

    setup prettier

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,23 @@
 // import
-
+// const { ethers } = require("ethers")
+const { ethers } = require("hardhat")
 // async main
-async function main() {}
+async function main() {
+    // require("hardhat") knows where the contracts dir is and able to pull content from dir
+    // require("ethers") does not know
+    // therefore, using hardhat is better
+    const SimpleStorageFactory = await ethers.getContractFactory(
+        "SimpleStorage"
+    )
+    console.log("Deploying Contract...")
+    const simpleStorage = await SimpleStorageFactory.deploy()
+    await simpleStorage.deployed()
+}
 
 // main
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
