af95eaca1593111dfaa88709f7be390a16179309
Mark Lin <61925580+Marklin2289@users.noreply.github.com>
2022-06-24 21:24:20 -0700

    added etherscan plugin and setup verify function

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,7 @@
 // import
 // const { ethers } = require("ethers")
-const { ethers } = require("hardhat")
+const { ethers, run, network } = require("hardhat")
+
 // async main
 async function main() {
     // require("hardhat") knows where the contracts dir is and able to pull content from dir
@@ -15,8 +16,42 @@ async function main() {
     // what's the private key?
     // what's the RPC URL ?
     console.log(`Deployed contract to ${simpleStorage.address}`)
+    // what happens when we deploy to our hardhat network ? ()
+    // console.log(network.config)
+    if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+        console.log("Waiting for block confirmations...")
+        // first wait for few blocks
+        await simpleStorage.deployTransaction.wait(6)
+        // then run verify()
+        await verify(simpleStorage.address, [])
+    }
+
+    const currentValue = await simpleStorage.retrieve()
+    console.log(`Current Value: ${currentValue}`)
+
+    // Update the current value
+    const transactionResponse = await simpleStorage.store(7)
+    await transactionResponse.wait(1)
+    const updatedValue = await simpleStorage.retrieve()
+    console.log(`Updated Value is : ${updatedValue}`)
 }
 
+// auto verify and publish contract on etherscan only using hardhat plugin (nomiclabs/hardhat-etherscan)
+async function verify(contractAddress, args) {
+    console.log("Verifying contract...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (error) {
+        if (error.message.toLowerCase().includes("already verified")) {
+            console.log("Already Verified")
+        } else {
+            console.log(error)
+        }
+    }
+}
 // main
 main()
     .then(() => process.exit(0))
